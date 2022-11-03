88e50db46376968a40cfe51c58908975892c88fb
pohjie <pohjie.pj@gmail.com>
2022-11-03 14:06:02 +0800

    Display 16 by 16 grid

---
+++ etch-a-sketch.js
@@ -0,0 +1,22 @@
+// create 16 by 16 grid
+function createGrid() {
+  const container = document.querySelector('#container');
+
+  for (let i = 0; i < 16; i++) {
+    // create a new row
+    let row = document.createElement('div');
+    row.classList.add('row');
+
+    for (let j = 0; j < 16; j++) {
+      // add each column grid within the row here
+      let square = document.createElement('div');
+      square.classList.add('square');
+
+      row.appendChild(square);
+    }
+
+    container.appendChild(row);
+  }
+}
+
+createGrid();
